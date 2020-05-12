a075dc1f9b8239b3c50f971221fef6c703edb22c
Aokitakeshi0721 <yahpan@gmail.com>
2020-05-12 21:19:47 +0900

    Switching thumbnail

---
+++ app/assets/javascripts/products_show.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.showThumbnails li').hover(function(){
+      //サムネのインデックスを取得
+      let index = $('.showThumbnails li').index(this);
+      //サムネの画像URLを取得
+      let imageurl = $('.showThumbnails li').eq(index).find('img').attr('src');
+      //元のメイン画像を保存しておく
+      defaultImage = $('img.showImage').attr('src');
+      //大画像URLの書き換え
+      $('img.showImage').attr('src',imageurl);
+  },function(){
+      //元のメイン画像に戻す
+      $('img.showImage').attr('src',defaultImage);
+  });
+});
