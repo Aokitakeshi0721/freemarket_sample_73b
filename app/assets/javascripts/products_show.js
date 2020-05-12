1db6be85ce0cf723dc5e9afb7d51ca43d2452626
Aokitakeshi0721 <yahpan@gmail.com>
2020-05-13 00:55:28 +0900

    popup001

--- app/assets/javascripts/products_show.js
+++ app/assets/javascripts/products_show.js
@@ -4,12 +4,27 @@ $(function(){
       let index = $('.showThumbnails li').index(this);
       //サムネの画像URLを取得
       let imageurl = $('.showThumbnails li').eq(index).find('img').attr('src');
-      //元のメイン画像を保存しておく
-      defaultImage = $('img.showImage').attr('src');
+      // //元のメイン画像を保存しておく
+      // defaultImage = $('img.showImage').attr('src');
       //大画像URLの書き換え
       $('img.showImage').attr('src',imageurl);
-  },function(){
-      //元のメイン画像に戻す
-      $('img.showImage').attr('src',defaultImage);
+  });
+});
+// ,function(){
+//   //元のメイン画像に戻す
+//   $('img.showImage').attr('src',defaultImage);
+// }
+
+$(function() {
+  $(".pop").click(function(e) {
+    e.preventDefault();
+    var id = $(this).attr("href");
+    console.log(id);
+    $(id).fadeIn();
+    $(".popupbg").fadeIn(1);
+  });
+  $(".popupbg,.close_btn").click(function() {
+    $(".popup_wrapper:visible").fadeOut();
+    $(".popupbg").fadeOut();
   });
 });
